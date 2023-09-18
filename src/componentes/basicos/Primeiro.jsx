f9ac45adc8ec8ade618b3d76d0843b9dcfaaf6d8
Guilherme3267 <Vyer3267@gmail.com>
2023-09-18 13:54:48 -0300

    Alterações no projeto

---
+++ src/componentes/basicos/Primeiro.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+function Primeiro() {
+  return (
+    <div>
+      <h1>Componente Primeiro aqui</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nisl neque, pharetra eleifend pretium ac, accumsan eu tellus. Duis faucibus.</p>
+      <button>Clique aqui</button>
+    </div>
+  );
+}
+
+export default Primeiro;
